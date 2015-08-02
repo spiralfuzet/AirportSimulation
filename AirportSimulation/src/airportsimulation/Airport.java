886bdc6ee18928a4afd50ca2709d1f82b8c386b1
sugeeism <evnagyokatomi@gmail.com>
2015-08-02 19:58:33 +0200

    Added airport class.

---
+++ AirportSimulation/src/airportsimulation/Airport.java
@@ -0,0 +1,18 @@
+
+package airportsimulation;
+
+/**
+ *
+ * @author tmate
+ */
+public class Airport {
+    
+    private final String id;
+    private final String aliasName;
+
+    public Airport(String id, String aliasName) {
+        this.id = id;
+        this.aliasName = aliasName;
+    }
+    
+}
